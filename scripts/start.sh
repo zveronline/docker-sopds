f12beec14ea57d973741187ece182d6f4bc672f2
zveronline <zveronline@zveronline.ru>
2017-01-29 00:29:15 +0300

    .

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-if [ ! -f /var/lib/pgsql/data/base ]; then
+if [ ! -f /var/lib/pgsql/data/PG_VERSION ] 
+then
 chown -R postgres:postgres /var/lib/pgsql
 su postgres -c "/usr/bin/pg_ctl -D /var/lib/pgsql/data initdb"
 su postgres -c "/usr/bin/pg_ctl -D /var/lib/pgsql/data -l /var/lib/pgsql/data/pg.log start"
@@ -8,14 +9,16 @@ sleep 30
 psql -U postgres -c "create database sopds"
 psql -U postgres -c "create user sopds with password 'sopds'"
 psql -U postgres -c "grant all privileges on database sopds to sopds"
+cd /sopds
+python3 manage.py migrate
+python3 manage.py sopds_util setconf SOPDS_ROOT_LIB '/library'
+python3 manage.py sopds_util setconf SOPDS_INPX_ENABLE 'True'
+python3 manage.py sopds_util setconf SOPDS_LANGUAGE ru-RU
 su postgres -c "/usr/bin/pg_ctl -D /var/lib/pgsql/data -l /var/lib/pgsql/data/pg.log stop"
 sleep 30
 fi
 su postgres -c "/usr/bin/pg_ctl -D /var/lib/pgsql/data -l /var/lib/pgsql/data/pg.log start"
 
 cd /sopds
-python3 manage.py migrate
-python3 manage.py sopds_util setconf SOPDS_ROOT_LIB '/library'
-python3 manage.py sopds_util setconf SOPDS_INPX_ENABLE 'True'
-python3 manage.py sopds_util setconf SOPDS_LANGUAGE ru-RU
+
 python3 manage.py sopds_server start & python3 manage.py sopds_scanner start
