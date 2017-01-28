83bf72637ecef753216a47edea7732f13bfbdc31
zveronline <zveronline@zveronline.ru>
2017-01-29 00:33:12 +0300

    .

--- scripts/start.sh
+++ scripts/start.sh
@@ -5,7 +5,7 @@ then
 chown -R postgres:postgres /var/lib/pgsql
 su postgres -c "/usr/bin/pg_ctl -D /var/lib/pgsql/data initdb"
 su postgres -c "/usr/bin/pg_ctl -D /var/lib/pgsql/data -l /var/lib/pgsql/data/pg.log start"
-sleep 30
+sleep 10
 psql -U postgres -c "create database sopds"
 psql -U postgres -c "create user sopds with password 'sopds'"
 psql -U postgres -c "grant all privileges on database sopds to sopds"
@@ -15,10 +15,8 @@ python3 manage.py sopds_util setconf SOPDS_ROOT_LIB '/library'
 python3 manage.py sopds_util setconf SOPDS_INPX_ENABLE 'True'
 python3 manage.py sopds_util setconf SOPDS_LANGUAGE ru-RU
 su postgres -c "/usr/bin/pg_ctl -D /var/lib/pgsql/data -l /var/lib/pgsql/data/pg.log stop"
-sleep 30
+sleep 10
 fi
 su postgres -c "/usr/bin/pg_ctl -D /var/lib/pgsql/data -l /var/lib/pgsql/data/pg.log start"
-
 cd /sopds
-
 python3 manage.py sopds_server start & python3 manage.py sopds_scanner start
