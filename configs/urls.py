3ef4c4652247376d13ee1f12a2a90d7e6980fa9a
ZveR <zveronline@zveronline.ru>
2018-11-07 11:20:10 +0300

    fix import django.contrib.auth.views.logout

---
+++ configs/urls.py
@@ -0,0 +1,31 @@
+"""sopds URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.9/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import url, include
+from django.contrib import admin
+from django.views.generic import RedirectView
+from django.urls import reverse_lazy
+
+
+from django.contrib.auth.views import logout_view
+
+urlpatterns = [
+    url(r'^opds/', include('opds_catalog.urls', namespace='opds')),
+    url(r'^web/', include('sopds_web_backend.urls', namespace='web')),
+    url(r'^admin/', admin.site.urls),
+    #url(r'^logout/$', logout, {'next_page':'/web/'},name='logout'),   
+    #url(r'^', include('sopds_web_backend.urls', namespace='web', app_name='opds_web_backend')),
+    url(r'^', RedirectView.as_view(url=reverse_lazy("web:main"))),
+]
